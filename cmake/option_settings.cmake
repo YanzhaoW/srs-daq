7a5ffe9188a8bb45a403e4c981576e9da1e24312
Yanzhao Wang <yannzhaow@gmail.com>
2024-11-08 17:23:42 +0100

    test on static build

--- cmake/option_settings.cmake
+++ cmake/option_settings.cmake
@@ -1,5 +1,6 @@
 option(USE_ROOT "Force to use ROOT dependency." OFF)
 option(NO_ROOT "Disable the usage of ROOT dependency." OFF)
+option(BUILD_STATIC "Disable the usage of ROOT dependency." OFF)
 
 # set(CONAN_INSTALL_ARGS
 #     "--build=missing;--settings=\"compiler.cppstd=23\""
