cab0fe2d93db65bcc7011ad9e78213b4f36d3ea5
Yanzhao Wang <yannzhaow@gmail.com>
2024-11-14 15:48:44 +0100

    remove libs from conda envs

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,47 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain
-import os
+
+BOOST_LIBS = (
+    "atomic",
+    "charconv",
+    "chrono",
+    "cobalt",
+    "container",
+    "context",
+    "contract",
+    "coroutine",
+    "date_time",
+    "exception",
+    "fiber",
+    "filesystem",
+    "graph",
+    "graph_parallel",
+    "iostreams",
+    "json",
+    "locale",
+    "log",
+    "math",
+    "mpi",
+    "nowide",
+    "process",
+    "program_options",
+    "python",
+    "random",
+    "regex",
+    "serialization",
+    "stacktrace",
+    "system",
+    "test",
+    "thread",
+    "timer",
+    "type_erasure",
+    "url",
+    "wave",
+)
+
+BOOST_OPTIONS = {f"without_{_name}": True for _name in BOOST_LIBS if _name not in ['thread', 'atomic', 'chrono', 'container', 'date_time', 'exception', 'system']}
+BOOST_OPTIONS.update({"shared": False})
+
 
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -12,9 +53,9 @@ class CompressorRecipe(ConanFile):
         self.requires("spdlog/1.14.1")
         self.requires("zpp_bits/4.4.24")
         self.requires("fmt/11.0.1", override=True)
-
-        if os.environ['CONDA_DEFAULT_ENV'] != 'srs-static':
-            self.requires("onetbb/2021.12.0")
+        self.requires("protobuf/5.27.0", options = {"with_zlib": True, "fPIC": True, "shared": False})
+        self.requires("boost/1.86.0", options = BOOST_OPTIONS)
+        self.requires("onetbb/2021.12.0")
 
     def generate(self):
         tc = CMakeToolchain(self)
